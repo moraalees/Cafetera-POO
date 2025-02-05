8462d40b0aa28e303146f8cd033716356bddd84e
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-02-05 16:57:32 +0100

    tazas de manera aleatoria creadas

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,28 @@
 package src
 
 fun main(){
-    val cafeteraSala = Cafetera("Sala")
-    val cafeteraCocina = Cafetera("Cocina", 750)
-    val cafeteraOficina = Cafetera("Oficina", 500, 200)
+    val cafetera1 = Cafetera("Sala")
+    val cafetera2 = Cafetera("Cocina", 750)
+    val cafetera3 = Cafetera("Oficina", 500, 200)
 
-    println(cafeteraSala)
-    println(cafeteraCocina)
-    println(cafeteraOficina)
+    println("CAFETERAS:")
+    println(cafetera1)
+    println(cafetera2)
+    println(cafetera3)
+    println("-------------------------------------------------------------------------")
+
+    val listaTazas = mutableListOf<Taza>()
+
+    var contador = 0
+
+    println("TAZAS:")
+    while (contador < 20){
+        contador += 1
+        val taza = Taza(color = Color.values().random(), capacidad = listOf(50, 75, 100).random()
+        )
+        listaTazas.add(taza)
+        println(taza)
+    }
+    println("------------------------------------------------------------")
+
 }
