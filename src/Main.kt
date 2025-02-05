baa4ecd3478288cb6317e7970a522b560847e728
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-02-05 17:31:21 +0100

    adición de un readln() que pregunte al usuario cuantas tazas quiere

--- src/Main.kt
+++ src/Main.kt
@@ -17,7 +17,7 @@ fun main(){
     val cafetera2 = Cafetera("Cocina", 750)
     val cafetera3 = Cafetera("Oficina", 500, 200)
 
-    println("**********************************************")
+    println("************************************************************************")
     println("CAFETERAS:")
     println(cafetera1)
     println(cafetera2)
@@ -25,10 +25,12 @@ fun main(){
 
     val listaTazas = mutableListOf<Taza>()
 
+    println("Escriba el número de tazas que quiere llenar:")
+    val numeroTazas = readln().toInt()
     var contador = 0
 
     println("TAZAS:")
-    while (contador < 30){
+    while (contador < numeroTazas){
         contador += 1
         val taza = Taza(color = Color.values().random(), capacidad = listOf(50, 75, 100).random()
         )
@@ -40,21 +42,23 @@ fun main(){
     cafetera2.vaciar()
     cafetera2.agregarCafe(cafetera2.capacidad/2)
     cafetera3.agregarCafe(400)
-    println("**********************************************")
+    println("************************************************************************")
 
     println(cafetera1)
     println(cafetera2)
     println(cafetera3)
 
-    println("**********************************************")
+    println("************************************************************************")
     for (taza in listaTazas){
         llenarTazas(cafetera1, cafetera2, cafetera3, taza)
     }
 
+    println("CAFETERAS:")
     println(cafetera1)
     println(cafetera2)
     println(cafetera3)
 
+    println("TAZAS:")
     for (taza in listaTazas){
         println(taza)
     }
